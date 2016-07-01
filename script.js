610bd10c08dd2a7b9caaf7fb67d34e165f9fdde4
patmallee <patmallee@gmail.com>
2016-07-01 18:18:19 -0400

    Add class toggle on arrow button hover

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var photostrip = $('#photostrip')
+var photostrip = $('#photostrip');
+
+
+function hoverExpand() {
+	this.animate({ width: "+=10px" });
+};
 
 function moveRight() {
 	if (photostrip.css("left") === "-1600px") {
@@ -6,7 +11,7 @@ function moveRight() {
 	} else {
 		photostrip.animate({ left: "-=400" });
 	}
-}
+};
 
 function moveLeft() {
 	if (photostrip.css("left") === "0px") {
@@ -14,11 +19,18 @@ function moveLeft() {
 	} else {
 		photostrip.animate({ left: "+=400" });
 	}
-}
+};
 
 $().ready(function() {
 	$('#leftarrow').click(moveLeft);
 	$('#rightarrow').click(moveRight);
+	$('#leftarrow').hover(function() {
+		$(this).toggleClass('hovered-arrow');
+	});
+	$('#rightarrow').hover(function() {
+		$(this).toggleClass('hovered-arrow');
+	});
+
 	$('#button1').click(function(){
 		photostrip.animate({ left: 0 });
 	});
